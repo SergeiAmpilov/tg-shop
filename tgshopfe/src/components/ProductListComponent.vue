08120107dec0b5a17e31d4889a15a0aec4ffcaef
Sergei Ampilov <sergei.ampilov@gmail.com>
2024-01-02 20:06:40 +0300

    product service and product list component

---
+++ tgshopfe/src/components/ProductListComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ subtitle }}</p>
+    <ul>
+      <li v-for="product in products" :key="product.id">
+        <span>{{ product.title }}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ProductList',
+  props: ['title', 'subtitle', 'products'],
+});
+</script>
--- tgshopfe/src/views/ShopView.vue
+++ tgshopfe/src/views/ShopView.vue
@@ -4,6 +4,12 @@
     <p>
       {{ b }}
     </p>
+    <hr>
+    <product-list
+      title="Список продуктов"
+      subtitle="Список алкогольных продуктов АСТ"
+      :products="products"
+    ></product-list>
   </div>
 </template>
 
@@ -13,16 +19,26 @@
 <script lang="ts">
 import { TelegramServiceProvider } from '@/services/Telegram.Service';
 import { defineComponent } from 'vue';
+import { ProductsService } from '@/services/Products.service';
+import ProductList from '@/components/ProductListComponent.vue';
+
+const prodService = new ProductsService();
 
 export default defineComponent({
   mounted() {
     TelegramServiceProvider.MainButton.setText('Главная кнопка');
     TelegramServiceProvider.MainButton.show();
+
+    console.log(prodService.byType);
   },
   data() {
     return {
       b: 14,
+      products: prodService.all,
     };
+  },
+  components: {
+    'product-list': ProductList,
   },
 });
 </script>
